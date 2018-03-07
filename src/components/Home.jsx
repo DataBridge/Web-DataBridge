fe35b6db2f3c3990b76982245a5ee0d1498250a8
JimZer <jimi.vaubien@protonmail.com>
2018-03-08 00:20:03 +0100

    landing page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -87,7 +87,6 @@ const Home = enhanceHome(({ styles, ...props }) => {
             <button 
               {...css(styles.firstContact)} 
               onClick={_ => props.setContact(true)}
-              id='contact-us'
             >
               CONTACT US
             </button>
